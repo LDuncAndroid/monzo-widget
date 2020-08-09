c3b6acde1e6be86f3ba2ecf646283944b5c1483d
Emma Vanbrabant <emmag87@gmail.com>
2020-08-09 13:08:53 +0100

    make input into setup client slightly less gross

---
+++ app/src/main/java/com/emmav/monzo/widget/common/Composables.kt
@@ -0,0 +1,67 @@
+package com.emmav.monzo.widget.common
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.ui.tooling.preview.Preview
+import java.util.*
+
+@Composable
+fun Info(
+    modifier: Modifier,
+    emoji: Text,
+    title: Text,
+    subtitle: Text
+) {
+    Column(modifier = modifier, horizontalGravity = Alignment.CenterHorizontally) {
+        Text(
+            text = ContextAmbient.current.resolveText(emoji),
+            fontSize = 84.sp
+        )
+        Text(
+            text = ContextAmbient.current.resolveText(title),
+            fontSize = 22.sp,
+            modifier = Modifier.padding(top = 32.dp)
+        )
+        Text(
+            text = ContextAmbient.current.resolveText(subtitle),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun InfoPreview() {
+    AppTheme {
+        Info(modifier = Modifier, emoji = text("🙌🏽"), title = text("title"), subtitle = text("subtitle"))
+    }
+}
+
+@Composable
+fun FullWidthButton(
+    onClick: () -> Unit,
+    title: Int,
+    enabled: Boolean = true
+) {
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+        modifier = Modifier.padding(bottom = 16.dp).fillMaxWidth()
+    ) {
+        Text(
+            text = ContextAmbient.current.getString(title).toUpperCase(Locale.getDefault()),
+        )
+    }
+}
--- app/src/main/java/com/emmav/monzo/widget/feature/setupclient/SetupClientActivity.kt
+++ app/src/main/java/com/emmav/monzo/widget/feature/setupclient/SetupClientActivity.kt
@@ -6,28 +6,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.ui.tooling.preview.Preview
 import com.emmav.monzo.widget.App
 import com.emmav.monzo.widget.R
 import com.emmav.monzo.widget.common.AppTheme
-import com.emmav.monzo.widget.common.Text
+import com.emmav.monzo.widget.common.FullWidthButton
+import com.emmav.monzo.widget.common.Info
 import com.emmav.monzo.widget.common.openUrl
-import com.emmav.monzo.widget.common.resolveText
 import com.emmav.monzo.widget.feature.login.LoginActivity
-import java.util.*
 
 class SetupClientActivity : AppCompatActivity() {
 
@@ -117,32 +111,6 @@ fun Content(
 }
 
 @Composable
-fun Info(
-    modifier: Modifier,
-    emoji: Text,
-    title: Text,
-    subtitle: Text
-) {
-    Column(modifier = modifier, horizontalGravity = Alignment.CenterHorizontally) {
-        Text(
-            text = ContextAmbient.current.resolveText(emoji),
-            fontSize = 84.sp
-        )
-        Text(
-            text = ContextAmbient.current.resolveText(title),
-            fontSize = 22.sp,
-            modifier = Modifier.padding(top = 32.dp)
-        )
-        Text(
-            text = ContextAmbient.current.resolveText(subtitle),
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = 16.dp)
-        )
-    }
-}
-
-@Composable
 fun Input(
     modifier: Modifier,
     state: SetupClientViewModel.State,
@@ -198,22 +166,5 @@ fun Actions(
                 )
             }
         }
-    }
-}
-
-@Composable
-fun FullWidthButton(
-    onClick: () -> Unit,
-    title: Int,
-    enabled: Boolean = true
-) {
-    Button(
-        onClick = onClick,
-        enabled = enabled,
-        modifier = Modifier.padding(bottom = 16.dp).fillMaxWidth()
-    ) {
-        Text(
-            text = ContextAmbient.current.getString(title).toUpperCase(Locale.getDefault()),
-        )
     }
 }
